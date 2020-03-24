19d199152a023baf46e25b30b78d49a68dbf1fb5
meggaclem <cndigitech@gmail.com>
2020-03-23 17:38:18 -0700

    first_webpage/script.js

---
+++ first_webpage/script.js
@@ -0,0 +1,15 @@
+let name= 'Clement Nduonyi'
+console.log('My name is ' +name);
+
+//courses array
+
+var courses= ['HTML', 'CSS', 'JS', 'PYTHON'];
+
+//Finding and displaying even numbers between 1 and 200
+
+fo(let i=2; i<=200; i++){
+	if(i%2===0){
+		courses.push(i);
+	}
+}
+console.log(courses);
