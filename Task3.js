4a7bca3ecfe1ddb7d33984852dbbe1889461fc00
meggaclem <cndigitech@gmail.com>
2020-04-20 00:56:21 -0700

    Modification

--- Task3.js
+++ Task3.js
@@ -1,16 +1,22 @@
+//Declaring A function to check the divisibilty of a numbers by 2,3 5
 function divisibilty_check(number){
+//output Array
   let output= [];
+    
+//For loop for the iteration
   for(let i=1; i<=number; i++){
+      
+//conditionals
     if (i % 2 === 0) {
             if (i%2===0 && i % 3 === 0 && i % 5 === 0) {
                 output.push("yu-gi-oh")
-            } else if ( i % 2=== 0 && i % 3===0) {
+            } else if (i % 2=== 0 && i % 3===0) {
                output.push("yu-gi")
             } else if (i%3===0 && i % 5 === 0) {
                output.push("gi-oh")
-            } else {
-              output.push("i")
-            }
+            } //else {
+              //output.push("i")
+            //}
         } else if (i % 3 === 0) {
             output.push("gi")
         } else if (i % 5 === 0) {
@@ -19,7 +25,9 @@ function divisibilty_check(number){
             output.push(i)
         }
     }
-      console.log(output);
+      //console.log(output);
      return output;
 }
-divisibilty_check(500);
+//Calling function with 100 as arguamnet and 80 as a random number of choice
+divisibilty_check(100);
+divisibilty_check(80);
